031e9a1e1caca6ba4477f6d1e01f1d191640c14b
Konsts <konst.st.96@hotmail.com>
2014-05-14 14:44:52 +0300

    added solution algoritm

---
+++ StankovK/ClassLibrary1/InsertionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    
+    public static class InsertionSort
+    {
+        
+        public static int[] ArraySort(int[] mas)
+        {
+            int x, j;
+            for (int i = 1; i < mas.Length; i++)
+            {
+                x = mas[i];
+                for (j = i - 1; j >= 0 && mas[j] > x; j--)
+                    mas[j + 1] = mas[j];
+
+                mas[j + 1] = x;
+            }
+            return mas;
+        }
+
+        public static int[] ArraySort(int[] mas, bool token)
+        {
+            for (int i = 1; i < mas.Length; i++)
+            {
+                int x = mas[i];
+                int j = i - 1;
+                while (j >= 0 && (token ? (mas[j] < x) : (mas[j] > x)))
+                {
+                    mas[j + 1] = mas[j];
+                    j--;
+                }
+                mas[j + 1] = x;
+            }
+            return mas;
+        }
+        public static int[] ArraySortDelegate(int[] mas, Func<int, int, bool> comparator )
+        {
+            for (int i = 1; i < mas.Length; i++)
+            {
+                int x = mas[i];
+                int j = i - 1;
+                while (j >= 0 && comparator (mas[j],x))
+                {
+                    mas[j + 1] = mas[j];
+                    j--;
+                }
+                mas[j + 1] = x;
+            }
+            return mas;
+        }
+    }
+}
---
+++ StankovK/SortTest/SortTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using NUnit.Framework;
+
+namespace SortTest
+{
+    [TestFixture]
+    class SortTest
+    {
+        private int[] GetNotSortArray
+        {
+            get
+            {
+                var random = new Random();
+                return Enumerable.Range(1, 10).OrderBy(r => random.Next()).ToArray();
+            }
+        }
+
+        [Test]
+        public void sortTest()
+        {
+            var NotSortArrey = GetNotSortArray;
+            var massive = NotSortArrey.OrderBy(x => x);
+            //var sort = new ClassLibrary1.InsertionSort();
+            //var result = sort.ArraySort(massiveSort);
+            var result = InsertionSort.ArraySort(NotSortArrey);
+            Assert.AreEqual(massive, result);
+        }
+
+        [Test]
+        public void SortTestTrenary()
+        {
+            var NotSortArrey = GetNotSortArray;
+            var massive = NotSortArrey.OrderBy(x => x);
+            var result = InsertionSort.ArraySort(NotSortArrey, false);
+            Assert.AreEqual(massive, result);
+        }
+        [Test]
+        public void SortTestTrenary_ByDescending()
+        {
+            var NotSortArrey = GetNotSortArray;
+            var massive = NotSortArrey.OrderByDescending(x => x);
+            var result = InsertionSort.ArraySort(NotSortArrey, true);
+            Assert.AreEqual(massive, result);
+        }
+
+        [Test]
+        public void SortTestTrenary_Delegate()
+        {
+            var NotSortArrey = GetNotSortArray;
+            var massive = NotSortArrey.OrderBy(x => x);
+            var result = InsertionSort.ArraySortDelegate(NotSortArrey, (x, y) => x > y);
+            Assert.AreEqual(massive, result);
+        }
+        [Test]
+        public void SortTestTrenary_Delegate_ByDescending()
+        {
+            var NotSortArrey = GetNotSortArray;
+            var massive = NotSortArrey.OrderByDescending(x => x);
+            var result = InsertionSort.ArraySortDelegate(NotSortArrey, (x, y) => x < y);
+            Assert.AreEqual(massive, result);
+        }
+
+
+    }
+}

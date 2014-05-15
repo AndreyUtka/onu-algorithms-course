0cd916d53c8377f3933195dc59509c4e65c7289f
Ruslan140293 <ruslan140293@ukr.net>
2014-05-15 20:56:55 +0300

    no message

---
+++ BelinskiiR/SortVst/Algorithms.SortVsrTest/InsertionSort Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SortVst;
+
+namespace Algorithms.SortVsrTest
+{
+    [TestFixture]
+    public class InsertionSortTest
+    {
+        private int[] GetNotSortedArray
+        {
+            get { var random = new Random();
+                return Enumerable.Range(1, 10).OrderBy(r => random.Next()).ToArray();}
+        }
+
+        [Test]
+        public void InsertionSortBase_NotSortedArray_SortedArrayByAscending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByAscending=notSortedArray.OrderBy(x => x);
+            //Act
+            var resultArray = InsertionSort.Sort(notSortedArray);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByAscending);
+        }
+        [Test]
+        public void InsertionSortWithTrinar_NotSortedArray_SortedArrayByAscending()
+        {
+            //Arrange
+            var notSortedArray = GetNotSortedArray;
+            var sortedArrayByAscending = notSortedArray.OrderBy(x => x);
+            //Act
+            var resultArray = InsertionSort.SortTrinar(notSortedArray,true);
+            //Assert
+            Assert.AreEqual(resultArray, sortedArrayByAscending);
+        }
+   
+    
+
+    }
+}

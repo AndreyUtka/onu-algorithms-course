64a4d8e6357af27af6ebe9c8c1672c3c872a1d8f
AndreyUtka <andreyutka@gmail.com>
2015-03-03 20:14:46 +0200

    added merge sort

---
+++ amichaluik/Algorithms/js/src/merge.sort.js
@@ -0,0 +1,53 @@
+var mergeSort = (function () {
+
+    /**
+     * Merge Sort with O(n log n) complexity
+     * @param {Array} array The array to sort
+     * @param {Function} comparator
+     */
+    function sort(array, comparator) {
+
+        if (array.length <= 1) {
+            return array;
+        }
+
+        var midpoint = Math.floor(array.length / 2),
+            left = array.slice(0, midpoint),
+            right = array.slice(midpoint, array.length);
+
+        return merge(sort(left, comparator), sort(right, comparator), comparator);
+    }
+
+    /**
+     * @param {Array} left The left hand sublist
+     * @param {Array} right The right hand sublist
+     * @param {Function} comparator
+     */
+    function merge(left, right, comparator) {
+
+        var result = [];
+
+        while (left.length && right.length) {
+            if (comparator(left[0], right[0]) <= 0) {
+                result.push(left.shift());
+            } else {
+                result.push(right.shift());
+            }
+        }
+
+        while (left.length)
+            result.push(left.shift());
+
+        while (right.length)
+            result.push(right.shift());
+
+        return result.concat(left, right);
+    }
+
+    return {
+        sort: sort
+    };
+
+})();
+
+module.exports = mergeSort.sort;
---
+++ amichaluik/Algorithms/js/test/merge.sort.spec.js
@@ -0,0 +1,28 @@
+var mergeSort = require('../src/merge.sort');
+var expect = require('chai').expect;
+
+describe('Merge Sort', function () {
+    it('should be sorted array by ascending', function () {
+        var notSortedArray = [5, 10, 9, 0, 6, 17];
+        var sortedArray = [0, 5, 6, 9, 10, 17];
+        var comparator = function (x, y) {
+            return x - y;
+        };
+
+        var resultArray = mergeSort(notSortedArray, comparator);
+
+        expect(resultArray).to.deep.equal(sortedArray);
+    });
+
+    it('should be sorted array by descending', function () {
+        var notSortedArray = [5, 10, 9, 0, 6, 17, 3];
+        var sortedArray = [17, 10, 9, 6, 5, 3, 0];
+        var comparator = function (x, y) {
+            return y - x;
+        };
+
+        var resultArray = mergeSort(notSortedArray, comparator);
+
+        expect(resultArray).to.deep.equal(sortedArray);
+    });
+});
